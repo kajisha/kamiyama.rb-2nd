843367c337b33efc1f1856595a281a45273296fc
kajisha <kajisha@gmail.com>
2015-09-22 16:11:42 +0900

    rename html filename

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           base: '.',
           livereload: true,
           open: {
-            target: 'http://localhost:' + port + '/boilerplate-local.html'
+            target: 'http://localhost:' + port + '/index.html'
           },
         }
       }
